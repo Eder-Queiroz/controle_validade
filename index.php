f00fc3311c290c36d78830895a487c663b552c3b
Eder Queiroz <edix122.edi@hotmail.com>
2022-01-28 19:39:24 -0300

    foi criado o formulario para editar a data

--- index.php
+++ index.php
@@ -57,6 +57,39 @@
 
 </nav>
 
+<main class="d-flex flex-row justify-content-center align-items-center">
+
+    <section class="container-md col-md-6">
+        
+        <form action="#" method="post" class="d-flex flex-column justify-content-center align-items-center">
+
+            <div class="row">
+                
+                <div class="form-group col-md-6">
+                    <label for="codeBar">Cod. Barra</label>
+                    <input type="number" name="codeBar" id="codeBar" class="form-control">
+                </div>
+
+                <div class="form-group col-md-6">
+                    <label for="unidade">Unidades</label>
+                    <input type="number" name="unidade" id="unidade" class="form-control">
+                </div>
+
+                <div class="form-group col-md-6 mt-3">
+                    <label for="data">Data de Vencimento</label>
+                    <input type="date" name="data" id="data" class="form-control">
+                </div>
+                
+            </div>
+
+            <button class="btn btn-lg mt-4">Enviar</button>
+
+        </form>
+
+    </section>
+
+</main>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
 </body>
