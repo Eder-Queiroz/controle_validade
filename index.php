5ec8eebba688c0c59290cb21d7d699360c9c2fab
Eder Queiroz <edix122.edi@hotmail.com>
2022-01-28 18:43:35 -0300

    foi criado o menu de navegacao

--- index.php
+++ index.php
@@ -12,6 +12,8 @@
     <!-- Links -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/style/style.css">
+    
+    <!-- Fotns -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Fira+Code&family=Roboto:wght@300;400;700&family=Space+Mono:wght@400;700&display=swap" rel="stylesheet">
@@ -19,7 +21,41 @@
 </head>
 <body>
     
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
 
+    <div class="container-fluid">
+
+        <a href="#" class="navbar-brand">
+            <img src="assets/images/logo.png" alt="Logo Super Maxi">
+        </a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse text-center mt-4" id="navbarNav">
+            
+            <ul class="navbar-nav mb-4">
+
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Add Validade</a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Lista Validade</a>
+                </li>
+
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Tabela</a>
+                </li>
+
+            </ul>
+            
+        </div>
+
+    </div>
+
+</nav>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
